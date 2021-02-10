235725736a3b97068209eabf8b17f1c4a8e79ee4
ks-kon <kskon@bk.ru>
2021-02-10 19:31:34 +0300

    repeat

--- n2/Source.cpp
+++ n2/Source.cpp
@@ -2,12 +2,6 @@
 
 int main()
 {
-	std::cout << "test" << std::endl;
-<<<<<<< HEAD
-	std::cout << "branch" << std::endl;
-=======
-	std::cout << "master" << std::endl;
->>>>>> > master
-std::cout << "add more commit" << std::endl;
+	std::cout << "repeat" << std::endl;
 	return EXIT_SUCCESS;
 }
